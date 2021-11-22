80f9be044f843fbfaf7878d46962f97787f7247d
pasindur2 <pasindur2pj@gmail.com>
2021-11-22 22:45:00 +0530

    Create change bgcolor component

---
+++ src/Organizations/index.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+
+const organisations = [
+    {
+        id: 5,
+        name: "Adoptees"
+    },
+    {
+        id: 6,
+        name: "ODEL"
+    }
+]
+
+function Organisations() {
+    return organisations.map((organisation) => {
+        return (
+            <div>
+                <div>
+                    <div key={organisation.id}>
+                        {organisation.name}
+                    </div>
+                </div>
+            </div>
+        );
+    })
+}
+
+export default Organisations;
