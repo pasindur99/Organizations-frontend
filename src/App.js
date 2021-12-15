870317caf01adfc82905aaf8c7291c1df5f0eb2c
pasindur2 <pasindur2pj@gmail.com>
2021-12-15 07:27:20 +0530

    Search bar in progress

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import './App.css';
 import Organisations from "./Organizations";
 import {useState} from "react";
-
-
+import {Col, Row} from "reactstrap";
 
 function App() {
     const [color, setColor] = useState("#008cc4");
+    const handleChange = event => {
+        console.log(event.target.value);
+    }
     return (
         <div>
-            <div style={{background:color}}>
-                <Organisations/>
-                <button onClick={() => setColor("Blue")}>
-                    Blue
-                </button>
-                <button onClick={() => setColor("Red")}>
-                    Red
-                </button>
+            <div className="App">
+                <h1>My Hacker Stories</h1>
+                <div style={{background: color}}>
+                    <button onClick={() => setColor("Blue")}>
+                        Blue
+                    </button>
+                    <button onClick={() => setColor("Red")}>
+                        Red
+                    </button>
+                </div>
+                <br/>
+                <label htmlFor="search">Search: </label>
+                <input id="search" type="text" onChange={handleChange}/>
+                <hr/>
             </div>
+            <Row>
+                <Col md={3}>
+                    <Organisations/>
+                </Col>
+            </Row>
         </div>
 
     );
--- src/Organizations/index.js
+++ src/Organizations/index.js
@@ -1,13 +1,18 @@
 import React from "react";
+import {Card, CardBody, CardSubtitle, CardText, CardTitle} from "reactstrap";
 
 const organisations = [
     {
-        id: 5,
-        name: "Adoptees"
+        "id": "1",
+        "name": "Adoptees"
     },
     {
-        id: 6,
-        name: "ODEL"
+        "id": "2",
+        "name": "PawLK"
+    },
+    {
+        "id": "3",
+        "name": "PetSo"
     }
 ]
 
@@ -15,11 +20,27 @@ function Organisations() {
     return organisations.map((organisation) => {
         return (
             <div>
-                <div>
-                    <div key={organisation.id}>
-                        {organisation.name}
-                    </div>
-                </div>
+                {/*<div>*/}
+                {/*    <div key={organisation.id}>*/}
+                {/*        {organisation.name}*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+                <Card>
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            {organisation.id}
+                        </CardTitle>
+                        <CardSubtitle
+                            className="mb-2 text-muted"
+                            tag="h6"
+                        >
+                            {organisation.name}
+                        </CardSubtitle>
+                        <CardText>
+                            {organisation.name}
+                        </CardText>
+                    </CardBody>
+                </Card>
             </div>
         );
     })
